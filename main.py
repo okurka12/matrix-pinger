f71228cd63e911b7cfee89cdf9b9508de299e25b
Vít Pavlík <pavlikvitek13@gmail.com>
2025-12-15 00:30:15 +0100

    add commands to list devices and verify devices

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 import niobot
+
+from nio.crypto.device import OlmDevice
+
 import config as cfg
 
 # Initialize the bot
@@ -11,7 +14,7 @@ bot = niobot.NioBot(
     homeserver=cfg.homeserver,
     user_id=cfg.bot_user_id,
     device_id=cfg.device_id,
-    owner_id=cfg.device_id,
+    owner_id=cfg.owner_id,
     store_path=cfg.store_path,
     auto_join_rooms=cfg.auto_join_rooms,
     auto_read_messages=cfg.auto_read_messages,
@@ -38,9 +41,66 @@ async def info_command(ctx: niobot.Context):
     )
 
 @bot.command(name="echo")
-async def echo_command(ctx: niobot.Context, message):
+async def echo_command(ctx: niobot.Context, message: str):
     """repeats message"""
     await ctx.respond(str(message))
+
+
+def lookup_device(user_id: str, device_id: str) -> OlmDevice|None:
+    for olmdevice in bot.device_store:
+        conditions = [
+            olmdevice.user_id == user_id,
+            olmdevice.device_id == device_id
+        ]
+        if all(conditions):
+            return olmdevice
+
+
+@bot.command(name="list-devices")
+async def list_devices_command(ctx: niobot.Context):
+    """list all known devices (owner only)"""
+    if not bot.is_owner(ctx.msg.sender):
+        await ctx.respond("only owner can do this")
+        return
+
+    output = ""
+    count = 0
+    for olmdevice in bot.device_store:
+        count += 1
+        output += f"User ID: {olmdevice.user_id}\n"
+        output += f"Device ID: {olmdevice.device_id}\n"
+        output += f"Display name: {olmdevice.display_name}\n"
+        output += f"Keys: {olmdevice.keys}\n"
+        output += f"Deleted: {olmdevice.deleted}\n"
+        output += f"Trust state: {olmdevice.trust_state}\n"
+        output += "-------------------------------------\n"
+
+    output += f"Total: {count}"
+
+    await ctx.respond(output, content_type="plain")
+
+
+@bot.command(name="verify-device")
+async def verify_device_command(
+    ctx: niobot.Context,
+    user_id: str,
+    device_id: str
+):
+    """verify a specific device key (owner only)"""
+    if bot.is_owner(ctx.msg.sender):
+
+        olmdevice = lookup_device(user_id, device_id)
+
+        if olmdevice is None:
+            await ctx.respond("couldn't find that device in my store")
+        else:
+            result = bot.verify_device(olmdevice)
+            if result:
+                await ctx.respond("successfuly verified")
+            else:
+                await ctx.respond("that device was already verified 🙄")
+    else:
+        await ctx.respond("only owner can do this")
 
 
 bot.run(password=cfg.password)
