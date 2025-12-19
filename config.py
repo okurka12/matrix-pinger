189489357a71a12707c77d66be233072009b806b
Vít Pavlík <pavlikvitek13@gmail.com>
2025-12-19 04:31:47 +0100

    update config documentation

--- config.py
+++ config.py
@@ -5,8 +5,12 @@ bot_user_id = "@test-user-xx:matrix.vitapavlik.cz"
 auto_join_rooms = True
 auto_read_messages = True
 
-# you can set pretty much anything here but keep it consistent so your bot
-# doesn't create a milion device keys
+#
+# you can set pretty much anything here, the bot will create a device key with
+# this id, all files needed to use that device key when the bot restarts will
+# be located in the store directory. keep it consistent and save the store
+# directory, so your bot doesn't have a milion device keys
+#
 device_id = "my-device-1234"
 
 # the device keys are stored here
